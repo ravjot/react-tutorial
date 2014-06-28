a7ed5b592719103d27817152196e330f5fc0c3a5
Ravjot Pasricha <ravjot@gmail.com>
2014-06-27 20:08:04 -0400

    Get this thing working out the box

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var fs = require('fs');
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
-var comments = [{author: 'Pete Hunt', text: 'Hey there!'}];
+var comments = [{author: 'Rav', text: 'I like stuff'}];
 
 app.use('/', express.static(__dirname));
-app.use(express.bodyParser());
+app.use(bodyParser());
 
 app.get('/comments.json', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
